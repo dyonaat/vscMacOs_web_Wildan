170ccdd0c2dbb46c6d7f942a16c7229542c8daab
alfathatlanta-hub <alfath.atlanta@gmail.com>
2026-01-01 20:24:52 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import {Navbar, Welcome} from './components'
+const App = () => {
+    return (
+        <main>
+            <Navbar />
+            <Welcome />
+        </main>
+    );
+};
+export default App
